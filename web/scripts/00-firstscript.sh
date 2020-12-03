56eeed2af514b803fa09f0052311e042f3f9d78b
Alan Lam <68211972+lam0819@users.noreply.github.com>
2020-12-03 18:42:22 +0800

    update cron job script

--- web/scripts/00-firstscript.sh
+++ web/scripts/00-firstscript.sh
@@ -4,4 +4,10 @@ echo "Start first script"
 #install new cron file
 
 # uncomment below script
-crontab -l | { cat; echo "* * * * * cd /var/www/html/src && php artisan schedule:run >> /dev/null 2>&1"; } | crontab -
+if [[ $(crontab -l | egrep -v "^(#|$)" | grep -q 'cd /var/www/html/src && php artisan schedule:run >> /dev/null 2>&1'; echo $?) == 1 ]]
+then
+    $(crontab -l | { cat; echo "* * * * * cd /var/www/html/src && php artisan schedule:run >> /dev/null 2>&1"; } | crontab -)
+fi
+#crontab -l | { cat; echo "* * * * * cd /var/www/html && php artisan schedule:run >> /dev/null 2>&1"; } | crontab -
+/usr/sbin/crond -l 8
+#crond restart
